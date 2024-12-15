732367a28e151b61fb2a70acc9c322dadf4a407e
Abdulnour <Abdulnour_Abdulrhman@outlook.com>
2024-12-15 23:47:25 +0300

    add: initial files

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../compnents/Hero";
+import Content from "../compnents/Content";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <Content />
+    </>
+  );
+};
+
+export default HomePage;
